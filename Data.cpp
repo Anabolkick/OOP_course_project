666d92947797db6d2df137ae72606dda1a1a509d
Beynichka <abeynik21@gmail.com>
2021-11-08 12:22:57 +0300

    22

--- Data.cpp
+++ Data.cpp
@@ -1,14 +1,196 @@
+//#include <iostream>
+//#include "Hash.h"
+//#include "Data.h"
+//
+//using namespace hash;
+//
+//
+//struct DataD {	//хранение даннных проголосовавшего
+//	string FullName;
+//	long ID;
+//	string HAsh;
+//};
+//
+//class Set {
+//	class Dyn
+//	{
+//	public:
+//		Dyn* pNext;	//хранение ссылок на следущие
+//		Dyn* pPrev;
+//		DataD Inf;
+//		Dyn(DataD In, Dyn* prev, Dyn* next = nullptr);
+//
+//	};
+//private:
+//	
+//	int size;
+//public:
+//	Dyn* Head;
+//	Set(Dyn* Head);
+//	~Set();
+//	Set(const Set& Rop);
+//	bool include(DataD V);//добавление в список
+//	void exclude(string H);//удаление со списка
+//	int CompHash(Dyn* Head, string H);//поиск по хешу
+//	int CompId(Dyn* Head, long Id);//поиск по ID
+//};
+//
+//
+//Set::Set(Dyn* Head)
+//{
+//	Head = nullptr;
+//	size = 0;
+//}
+//
+//Set::~Set()
+//{
+//	Dyn* Tdelete;
+//	do {
+//		Tdelete = Head;
+//		Head = Tdelete->pNext;
+//		delete Tdelete;
+//	} while (Head != nullptr);
+//}
+//
+//
+//Set::Set(const Set& Rop)
+//{
+//	Head = Rop.Head;
+//	size = Rop.size;
+//}
+//
+//
+//
+//int Set::CompHash(Dyn* Head, string H)//сравнение по хешу
+//{
+//	Hash h1;
+//	string str;
+//	str = h1.getHash(H, 16);
+//	Dyn* tmp, * current;
+//	tmp = Head;
+//	int c = 0;
+//	while (tmp != nullptr) {
+//		if (tmp->Inf.HAsh != str) {
+//			c++;
+//		}
+//		else {
+//			c = -1;
+//			break;
+//		}
+//		current = tmp;
+//
+//		tmp = tmp->pNext;
+//	}
+//	if (current->Inf.HAsh != str) { c = -1; }
+//	return c;
+//}
+//
+//int Set::CompId(Dyn* Head, long Id)//сравнение по id
+//{
+//	Dyn* tmp, * current;
+//	tmp = Head;
+//	int c = 0;
+//	while (tmp != nullptr) {
+//		if (tmp->Inf.ID != Id) {
+//			c++;
+//		}
+//		else {
+//			c = -1;
+//			break;
+//		}
+//		current = tmp;
+//
+//		tmp = tmp->pNext;
+//	}
+//	if (current->Inf.ID != Id) { c = -1; }
+//	return c;
+//
+//}
+//
+//bool Set::include(DataD V) //добавление нового элемента
+//{
+//	if (Head == nullptr) {
+//		Head = new Dyn(V, nullptr);
+//		++size;
+//		return true;
+//	}
+//	else if (CompId(Head, V.ID) != -1) {
+//		Dyn* current = Head;
+//		while (current->pNext != nullptr) {
+//			current = current->pNext;
+//		}
+//		current->pNext = new Dyn(V, current);
+//		++size;
+//		return true;
+//	}
+//	else
+//		return false;
+//}
+//
+//void Set::exclude(string H)//удаление по хешу
+//{
+//	if (size == 1 && Head->Inf.HAsh == H) {
+//		delete[] Head;
+//		Head = nullptr;
+//	}
+//	else if (Head->Inf.HAsh == H) {
+//		Dyn* current = Head;
+//		Head = current->pNext;
+//		Head->pPrev = nullptr;
+//		delete[] current;
+//	}
+//	else if (CompHash(Head, H) !=-1)
+//	{
+//		Dyn* current = Head, * prev = nullptr,*tmp;
+//		while (current->Inf.HAsh != H) {
+//			prev = current;
+//			current = current->pNext;
+//		}
+//		prev->pNext = current->pNext;
+//		tmp = current->pNext;
+//		tmp->pPrev = prev;
+//		delete current;
+//		
+//	}
+//	else cout << "no hash";
+//}
+//
+//
+//Set::Dyn::Dyn(DataD In, Dyn* prev, Dyn* next)
+//{
+//	Inf = In;
+//	pPrev = prev;
+//	pNext = next;
+//}
+//
+//Voting Rez(Voting* rez)
+//{
+//
+//	return Voting();
+//}
+//
+//Set::Set(Dyn* Head)
+//{
+//}
+
+
 #include <iostream>
 #include "Hash.h"
-#include "Data.h"
+
 
 using namespace hash;
 
+class Voting {
+public:
+	string NameC;
+	int point;
+};
 
 struct DataD {	//хранение даннных проголосовавшего
 	string FullName;
 	long ID;
 	string HAsh;
+	string VOTE;
 };
 
 class Set {
@@ -22,23 +204,25 @@ class Set {
 
 	};
 private:
-	
+
 	int size;
 public:
 	Dyn* Head;
-	Set(Dyn* Head);
+	Set();
 	~Set();
 	Set(const Set& Rop);
 	bool include(DataD V);//добавление в список
 	void exclude(string H);//удаление со списка
 	int CompHash(Dyn* Head, string H);//поиск по хешу
 	int CompId(Dyn* Head, long Id);//поиск по ID
+	friend Voting Rez(Voting* rez);
 };
 
 
-Set::Set(Dyn* Head)
+Set::Set()
 {
 	Head = nullptr;
+
 	size = 0;
 }
 
@@ -139,9 +323,9 @@ void Set::exclude(string H)//удаление по хешу
 		Head->pPrev = nullptr;
 		delete[] current;
 	}
-	else if (CompHash(Head, H) !=-1)
+	else if (CompHash(Head, H) != -1)
 	{
-		Dyn* current = Head, * prev = nullptr,*tmp;
+		Dyn* current = Head, * prev = nullptr, * tmp;
 		while (current->Inf.HAsh != H) {
 			prev = current;
 			current = current->pNext;
@@ -150,7 +334,7 @@ void Set::exclude(string H)//удаление по хешу
 		tmp = current->pNext;
 		tmp->pPrev = prev;
 		delete current;
-		
+
 	}
 	else cout << "no hash";
 }
@@ -161,14 +345,4 @@ Set::Dyn::Dyn(DataD In, Dyn* prev, Dyn* next)
 	Inf = In;
 	pPrev = prev;
 	pNext = next;
-}
-
-Voting Rez(Voting* rez)
-{
-
-	return Voting();
-}
-
-Set::Set(Dyn* Head)
-{
 }
--- MyForm.h
+++ MyForm.h
@@ -2,7 +2,6 @@
 #include "csv_manipulator.cpp"
 #include "string_manipulator.cpp"
 #include "Data.h"
-#using "Data.cpp"
 
 
 namespace Project1 {
@@ -330,7 +329,7 @@ namespace Project1 {
 		}
 #pragma endregion
 
-
+		Voting* Candidate = new Voting[3];
 		Set* Vote=new Set[1];//хранение списка
 	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
 	}
@@ -339,9 +338,7 @@ namespace Project1 {
 	private: System::Void label3_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
 	private: System::Void searchButton_Click(System::Object^ sender, System::EventArgs^ e) {
-
 		
-
 		if ( searchByID->Text!="") { /*Vote[0].CompId(Vote.Head, searchByID->Text)!=-1*/
 
 			MessageBox::Show(this, "ID [number] voted for [full name]", "Found!", MessageBoxButtons::OK, MessageBoxIcon::Information);
